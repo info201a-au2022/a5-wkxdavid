27e15697df9dbc7cce50fafeaf8a869f87a8cfd2
wkxdavid <dpham722@uw.edu>
2022-12-11 23:17:06 -0800

    partial commit

--- source/server.R
+++ source/server.R
@@ -1,15 +1,9 @@
-library(rsconnect)
-library(ggplot2)
-library(shiny)
 library(tidyverse)
 library(plotly)
-library(gganimate)
-library(shinycssloaders)
-library(thematic)
-library(shinythemes)
+library(shiny)
+library(rsconnect)
 
 server <- function(input, output) {
-  thematic_shiny()
 
   # loads the dataset from github
 
@@ -24,46 +18,39 @@ server <- function(input, output) {
                           share_global_luc_co2, cumulative_luc_co2, ghg_excluding_lucf_per_capita,
                           ghg_per_capita, share_global_cumulative_co2_including_luc, total_ghg)
   
-
   # when is the average land use capita per country
   average_land_use_capita <- land_use_data %>%
     select(country, year, land_use_change_co2_per_capita) %>%
     group_by(country) %>%
-    summarize(land_use_change_co2_per_capita = max(land_use_change_co2_per_capita,
-                                                    na.rm = TRUE))
-
-  # gets the max all colums
+    summarize(land_use_change_co2_per_capita = mean(land_use_change_co2_per_capita,
+                                                   na.rm = TRUE))
+  
+  # gets the highest total cumulative land use c02
+  # What the highest land-use c02 output?
   cumulative_land_use <- land_use_data %>%
     select(country, year, cumulative_luc_co2) %>%
     group_by(country) %>%
-    summarize(cumulative_luc_co2 = max(cumulative_luc_co2,na.rm = TRUE))
-
+    summarize(cumulative_luc_co2 = sum(cumulative_luc_co2))
+  
   # percent change
   percent_change_land_use <- land_use_data %>%
     arrange(year) %>%
     group_by(year) %>%
     mutate(pct_change = (lag(share_global_cumulative_co2_including_luc) -
                            share_global_cumulative_co2_including_luc)/
-                          share_global_cumulative_co2_including_luc * 100)
-  
-  output$yearChoice <- renderUI({
-    selectInput("Year",
-                label = "Select a Year",
-                choices = unique(interactive_data$year),
-                mutiple=FALSE)
-    
-  })
+             share_global_cumulative_co2_including_luc * 100)
   
   
-  output$barchart <- renderPlot({
-    barplot(interactive_data[,input$region], 
-            main=input$region,
-            ylab=input$region,
+  # the bar chart
+  output$chart <- renderPlot({
+    barplot(interactive_data[,input$type], 
+            main=input$type,
+            ylab=input$type,
             xlab="Countries")
   })
   
+  # creative picture
   output$home_pic <- renderImage({
     list(src ="land-use.png", width = "100%", height = 400)
   }, deleteFile = F)
 }
-
--- source/ui.R
+++ source/ui.R
@@ -1,26 +1,21 @@
-library(rsconnect)
-library(ggplot2)
-library(shiny)
 library(tidyverse)
-library(ggmap)
 library(plotly)
-library(gganimate)
-library(shinycssloaders)
-library(thematic)
+library(shiny)
+library(rsconnect)
 
 # widgets
 
 data_file <- read.csv("owid-co2-data.txt")
- 
-land_use_data <- select(data_file, land_use_change_co2, land_use_change_co2_per_capita,
-                        share_global_luc_co2, cumulative_luc_co2, ghg_excluding_lucf_per_capita,
-                        ghg_per_capita, share_global_cumulative_co2_including_luc, total_ghg)
+
 # text
-introduction_text <- "The focus of this project is the c02 emissions of land use. This is to explore
-                      how land use may be affecting the change in climate change. One relevant value of interest
-                      is what is the average land use per capita. For the world, it is 2.464 tonnes per person. Another relevant value 
-                      of interest is what is the cumulative land use across the world which is 742490.60 tonnes total.
-                      An third area of interest is what was the percent change of land use in North America. 
+introduction_text <- "The focus of this project is the CO2 emissions of land use. This is to explore
+                      how land use may be affecting the change in climate change. The dataset used in this project
+                      is from a github repoistory (https://github.com/owid/co2-data/). The dataset shows CO2 and greenhouse
+                      emissions for many categories. Though this project will only be focusing on land-use emissions."
+                      
+relevant_values <- "The first relevant value of interest is what is the average land-use per capita. For the world, it is 2.464 tonnes per person. An second relevant value 
+                      of interest is what is the cumulative land-use across the world which is 742490.60 tonnes total.
+                      An third area of interest is what was the percent change of land-use in North America. 
                       And in 2019 to 2020, it had a 1.123 % change."
 
 interactive_text <- "This bar chart is included to graph the varying levels of co2 via land change. 
@@ -29,12 +24,23 @@ interactive_text <- "This bar chart is included to graph the varying levels of c
                      where some of them are very low and others go really high. This shows that different
                      countries and areas have vastly different c02 land use emissions."
 
+  
+land_use_rename <- c("Cumulative production based Co2" = "share_global_cumulative_co2_including_luc",
+                     "Cumulative land use CO2" = "cumulative_luc_co2",
+                     "Annual production of CO2 from land-use per capita" = "land_use_change_co2_per_capita",
+                     "Annual production of CO2 based on territory " = "land_use_change_co2",
+                     "Cumulative land-use emissions shared" = "share_global_luc_co2")
+
+person_text <- "INFO 201 Fall 2022 A-5 - David Pham"
+
 # introduction page
 introduction <- tabPanel(
   "Introduction",
   imageOutput("home_pic"),
   titlePanel("Introduction"),
   p(introduction_text),
+  p(relevant_values),
+  p(person_text)
 )
 
 # interactive page
@@ -43,16 +49,22 @@ interactive <- tabPanel(
   titlePanel("Land Use per Capita"),
   sidebarLayout(
     sidebarPanel(
-      selectInput("region", "co2 type:", 
-                  choices=colnames(land_use_data)),
+      selectInput("type", "Select a Emission type:", 
+                  choices=land_use_rename), 
+      
+      selectInput("Country",
+                  label = "Select a Country",
+                  choices = unique(data_file$country)),
       p(interactive_text)
     ),
-    mainPanel(plotOutput("barchart"))
+    mainPanel(plotOutput("chart")),
+
   )
 )
 
+# navigation bar
 shinyui <- navbarPage(
-    "Land use c02 emissions",
+    "Land-Use Co2 Emissions",
     introduction,
-    interactive,
+    interactive
 )
